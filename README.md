243c5422e39ab2722ab703f7d53403660ddb45e3
VicoVillca <vicovillca@hotmail.com>
2023-02-09 08:36:51 -0400

    agregamos enlaces de acceso al repo

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # backend-goldprice
+https://gold-price.fly.dev/
+# Logs
+https://fly.io/apps/gold-price/monitoring
+
+# Code Util for FLY
+flyctl launch // subimos cambios al repositrio
+flyctl open // open repopsitory final
--- src/index.js
+++ src/index.js
@@ -4,10 +4,13 @@ const jsdom = require("jsdom");
 const app = express();
 const port = process.env.PORT || 3000;
 console.log("inciamos");
-app.get("/prueba", (req, res) => {
+app.get("/", (req, res) => {
+  console.log("saludamos");
   res.send("Hello World!");
 });
+
 app.get("/oro", async (req, res) => {
+  console.log("consultamos le precio del oro");
   try {
     // Abrimos una instancia del puppeteer y accedemos a la url de google
     const browser = await puppeteer.launch();
@@ -28,7 +31,6 @@ app.get("/oro", async (req, res) => {
         const element = c[i];
         const asqui = element.charCodeAt(0);
         if (asqui != 9 && asqui != 10 && asqui != 32) {
-          //console.log(typeof element+"-"+element+"("+element.charCodeAt(0)+")");
           x = x + element;
           sw = 1;
         } else {
