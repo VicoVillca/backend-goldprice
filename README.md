c80004231b8f24f5b7056f7c4ad3e55cc0d3c5bc
VicoVillca <vicovillca@hotmail.com>
2023-02-09 13:10:59 -0400

    agragamos config COR

--- README.md
+++ README.md
@@ -2,9 +2,14 @@
 
 gold-price
 https://gold-price.fly.dev/
+
+# Util
+
+ https://gold-price.fly.dev/prueba
 # Logs
 https://fly.io/apps/gold-price/monitoring
 
 # Code Util for FLY
 flyctl launch // subimos cambios al repositrio
+flyctl status // estado del backen
 flyctl open // open repopsitory final
--- src/index.js
+++ src/index.js
@@ -1,16 +1,20 @@
 const express = require("express");
-
+var cors = require("cors");
 const axios = require("axios");
 const cheerio = require("cheerio");
 const app = express();
+app.use(cors());
 const port = process.env.PORT || 3000;
 console.log("inciamos");
+//ASIGNAMOS RUTAS DONDE CONSULTAR
+
+//AGREGAMOS LAS OPERACIOENS UTILES
 
 app.get("/", (req, res) => {
   console.log("saludamos");
   res.send("Hello World! :)");
 });
-app.get("/prueba", (req, res) => {
+app.post("/prueba",  (req, res) => {
   console.log("prueba");
   try {
     axios.get("https://www.kitco.com/gbl/es/index.html").then(({ data }) => {
@@ -29,15 +33,12 @@ app.get("/prueba", (req, res) => {
         .toArray();
       //res.send(pokemonNames[0]);
       if (fecha.length > 0 && pokemonNames.length > 1) {
-        res.send(
-          "{'fecha':'" +
-            fecha[0] +
-            "','compra':" +
-            pokemonNames[0] +
-            ",'venta':" +
-            pokemonNames[1] +
-            "}"
-        );
+        const data = {
+          fecha: fecha[0],
+          compra: pokemonNames[0],
+          venta: pokemonNames[1],
+        };
+        res.send(JSON.stringify(data));
       } else {
         res.send("elementos no encontrados");
       }
