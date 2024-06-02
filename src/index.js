97977b9993f97ca9d23ab44e1cbe410f9f088d73
VicoVillca <vicovillca@hotmail.com>
2024-06-01 21:24:25 -0400

    Actualizamos a v2

--- src/index.js
+++ src/index.js
@@ -2,52 +2,61 @@ const express = require("express");
 const cors = require("cors");
 const axios = require("axios");
 const cheerio = require("cheerio");
+
 const app = express();
 app.use(cors());
+
 const port = process.env.PORT || 3000;
-console.log("inciamos");
-//ASIGNAMOS RUTAS DONDE CONSULTAR
 
-//AGREGAMOS LAS OPERACIOENS UTILES
+// Función para verificar si una cadena contiene puntos y números
+function contienePuntosYNumeros(cadena) {
+  const tienePunto = /\./;
+  const tieneNumero = /\d/;
+  return tienePunto.test(cadena) && tieneNumero.test(cadena);
+}
 
+// Ruta raíz
 app.get("/", (req, res) => {
   console.log("saludamos");
   res.send("Hello World! :)");
 });
-app.get("/prueba",  (req, res) => {
+
+// Ruta de prueba
+app.get("/prueba", async (req, res) => {
   console.log("prueba");
   try {
-    axios.get("https://www.kitco.com/gbl/es/index.html").then(({ data }) => {
-      const $ = cheerio.load(data);
-      const fecha = $("b")
-        .map((_, product) => {
-          const $product = $(product);
-          return $product.text();
-        })
-        .toArray();
-      const pokemonNames = $("td.data")
-        .map((_, product) => {
-          const $product = $(product);
-          return $product.text();
-        })
-        .toArray();
-      //res.send(pokemonNames[0]);
-      if (fecha.length > 0 && pokemonNames.length > 1) {
-        const data = {
-          fecha: fecha[0],
-          compra: pokemonNames[0],
-          venta: pokemonNames[1],
-        };
-        res.send(JSON.stringify(data));
-      } else {
-        res.send("elementos no encontrados");
-      }
-    });
+    const { data } = await axios.get("https://www.kitco.com");
+    const $ = cheerio.load(data);
+    const hhh = $("h3")
+      .map((_, product) => $(product).text())
+      .toArray();
+
+    const filtrado = hhh.filter(contienePuntosYNumeros);
+
+    // Imprimir el resultado filtrado
+    console.log(filtrado);
+    console.log("holap");
+
+    if (filtrado.length > 1) {
+      const fecha = new Date();
+      const responseData = {
+        success: true,
+        data: {
+          fecha: fecha.toISOString(),
+          compra: filtrado[0],
+          venta: filtrado[1],
+        },
+      };
+      res.json(responseData);
+    } else {
+      res.json({ success: false, message: "Datos no encontrados!" });
+    }
   } catch (error) {
-    console.log(error);
-    res.send(error);
+    console.error("Error:", error.message);
+    res.json({ success: false, message: "Error al obtener los datos" });
   }
 });
 
-app.listen(port);
-console.log("server on port " + port);
+app.listen(port, () => {
+  console.log("Servidor escuchando en el puerto " + port);
+});
